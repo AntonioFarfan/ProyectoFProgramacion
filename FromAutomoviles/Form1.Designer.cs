951db4e57d3b8898cba3a05254a247bef9c87fad
AntonioFarfan <70988355+AntonioFarfan@users.noreply.github.com>
2020-09-10 19:24:07 -0500

    Interfaz Grafica

---
+++ FromAutomoviles/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace FromAutomoviles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbModelo = new System.Windows.Forms.ComboBox();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.mtxtKilometraje = new System.Windows.Forms.MaskedTextBox();
+            this.txtDueño = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.pbAutos = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(249, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ford";
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(128, 267);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 2;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Placa:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Kilometraje:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(75, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Dueño:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(75, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Modelo:";
+            // 
+            // cbModelo
+            // 
+            this.cbModelo.FormattingEnabled = true;
+            this.cbModelo.Items.AddRange(new object[] {
+            "Raptor",
+            "Ranger",
+            "Explorer",
+            "Edge",
+            "Focus",
+            "Fiesta",
+            "B-Max"});
+            this.cbModelo.Location = new System.Drawing.Point(163, 71);
+            this.cbModelo.Name = "cbModelo";
+            this.cbModelo.Size = new System.Drawing.Size(121, 21);
+            this.cbModelo.TabIndex = 8;
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(163, 112);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(100, 20);
+            this.txtPlaca.TabIndex = 9;
+            // 
+            // mtxtKilometraje
+            // 
+            this.mtxtKilometraje.Location = new System.Drawing.Point(163, 157);
+            this.mtxtKilometraje.Mask = "9999999999999999";
+            this.mtxtKilometraje.Name = "mtxtKilometraje";
+            this.mtxtKilometraje.Size = new System.Drawing.Size(100, 20);
+            this.mtxtKilometraje.TabIndex = 10;
+            this.mtxtKilometraje.ValidatingType = typeof(int);
+            // 
+            // txtDueño
+            // 
+            this.txtDueño.Location = new System.Drawing.Point(163, 200);
+            this.txtDueño.Name = "txtDueño";
+            this.txtDueño.Size = new System.Drawing.Size(100, 20);
+            this.txtDueño.TabIndex = 11;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(254, 267);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 12;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            // 
+            // pbAutos
+            // 
+            this.pbAutos.Location = new System.Drawing.Point(431, 55);
+            this.pbAutos.Name = "pbAutos";
+            this.pbAutos.Size = new System.Drawing.Size(265, 178);
+            this.pbAutos.TabIndex = 13;
+            this.pbAutos.TabStop = false;
+            this.pbAutos.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 329);
+            this.Controls.Add(this.pbAutos);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtDueño);
+            this.Controls.Add(this.mtxtKilometraje);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.cbModelo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbAutos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbModelo;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.MaskedTextBox mtxtKilometraje;
+        private System.Windows.Forms.TextBox txtDueño;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.PictureBox pbAutos;
+    }
+}
+
---
+++ FromAutomoviles/Form2.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace FromAutomoviles
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMostrar = new System.Windows.Forms.DataGridView();
+            this.Modelo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Placa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kilometraje = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dueño = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMostrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMostrar
+            // 
+            this.dgvMostrar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMostrar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Modelo,
+            this.Placa,
+            this.Kilometraje,
+            this.Dueño});
+            this.dgvMostrar.Location = new System.Drawing.Point(38, 36);
+            this.dgvMostrar.Name = "dgvMostrar";
+            this.dgvMostrar.Size = new System.Drawing.Size(442, 132);
+            this.dgvMostrar.TabIndex = 0;
+            // 
+            // Modelo
+            // 
+            this.Modelo.HeaderText = "Modelo";
+            this.Modelo.Name = "Modelo";
+            // 
+            // Placa
+            // 
+            this.Placa.HeaderText = "Placa";
+            this.Placa.Name = "Placa";
+            // 
+            // Kilometraje
+            // 
+            this.Kilometraje.HeaderText = "Kilometraje";
+            this.Kilometraje.Name = "Kilometraje";
+            // 
+            // Dueño
+            // 
+            this.Dueño.HeaderText = "Dueño";
+            this.Dueño.Name = "Dueño";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 269);
+            this.Controls.Add(this.dgvMostrar);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMostrar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMostrar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Modelo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Placa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kilometraje;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dueño;
+    }
+}
