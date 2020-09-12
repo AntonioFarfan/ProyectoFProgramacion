ab4eca847bb64067f27aeb94fc4be7653e1e1cd4
AntonioFarfan <70988355+AntonioFarfan@users.noreply.github.com>
2020-09-11 20:14:48 -0500

    Creacion de la clase (#2)
    Co-authored-by: willian pandi <willian.pandi@unach.edu.ec>

---
+++ FromAutomoviles/Automoviles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromAutomoviles
+{
+    class Automoviles
+    {
+		public string Modelo { set; get; }
+		public string Placa { set; get; }
+		public int Kilometraje { set; get; }
+		public string Dueño { set; get; }
+
+		public Automoviles()
+		{
+		}
+
+		public Automoviles(string modelo, string placa, int kilometraje, string dueño)
+		{
+			Modelo = modelo;
+			Placa = placa;
+			Kilometraje = kilometraje;
+			Dueño = dueño;
+		}
+	}
+}
